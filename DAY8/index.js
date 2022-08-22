f678db58ae5aa2e7a6599dfa8c5bdd573622655c
Leah Deshar <leah.desar@gmail.com>
2022-08-22 07:07:30 +0545

    changes

--- DAY8/index.js
+++ DAY8/index.js
@@ -134,21 +134,21 @@ function displayMenuButtons() {
   const filterBtns = btnContainer.querySelectorAll(".filter-btn");
   console.log(filterBtns);
 
-//   filterBtns.forEach(function (btn) {
-//     btn.addEventListener("click", function (e) {
-//       // console.log(e.currentTarget.dataset);
-//       const category = e.currentTarget.dataset.id;
-//       const menuCategory = menu.filter(function (menuItem) {
-//         // console.log(menuItem.category);
-//         if (menuItem.category === category) {
-//           return menuItem;
-//         }
-//       });
-//       if (category === "all") {
-//         diplayMenuItems(menu);
-//       } else {
-//         diplayMenuItems(menuCategory);
-//       }
-//     });
-//   });
+  //   filterBtns.forEach(function (btn) {
+  //     btn.addEventListener("click", function (e) {
+  //       // console.log(e.currentTarget.dataset);
+  //       const category = e.currentTarget.dataset.id;
+  //       const menuCategory = menu.filter(function (menuItem) {
+  //         // console.log(menuItem.category);
+  //         if (menuItem.category === category) {
+  //           return menuItem;
+  //         }
+  //       });
+  //       if (category === "all") {
+  //         diplayMenuItems(menu);
+  //       } else {
+  //         diplayMenuItems(menuCategory);
+  //       }
+  //     });
+  //   });
 }
