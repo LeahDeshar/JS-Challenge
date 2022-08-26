400458921458fa69452d774b60403ee95ee07bdc
Leah Deshar <leah.desar@gmail.com>
2022-08-26 13:41:03 +0545

    prc.js --> reduce exercise

--- DAY8/PRC/prc.js
+++ DAY8/PRC/prc.js
@@ -173,4 +173,74 @@ const total = voters.reduce(function (acc, item) {
     return acc; //if false just return acc value as it is
   }
 }, 0);
-console.log(`There are ${total} voters.`); //display the length
+console.log(`There are ${total} voters.`);
+//display the length
+
+//4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
+
+const wishlist = [
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
+];
+
+const pick = wishlist.reduce(function (acc, item) {
+  return acc + item.price;
+}, 0);
+
+console.log(pick);
+
+// 5) Given an array of arrays, flatten them into a single array
+const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
+
+const singleArray = arrays.reduce(function (a, c) {
+  return a.concat(c);
+});
+console.log(singleArray);
+
+//6) Given an array of potential voters, return an object representing the results of the vote
+//Include how many of the potential voters were in the ages 18-25,
+// how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted.
+// The resulting object containing this data should have 6 properties.
+
+//Implementation
+// -- > we need to check where the peer group belongs to based on the age
+// -- > also increase the voting data for their corresponding group if they voted.
+
+const voter = [
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
+];
+const initialVotes = {
+  numYoungVotes: 0,
+  numYoungPeople: 0,
+  numMidVotesPeople: 0,
+  numMidPeople: 0,
+  numOldVotesPeople: 0,
+  numOldPeople: 0,
+};
+const potential_voters = voter.reduce(function (acc, item) {
+  if (item.age >= 18 && item.age <= 25) {
+    return acc.numYoungPeople++;
+  }
+  if (item.age >= 26 && item.age <= 35) {
+    return acc.numMidPeople++;
+  }
+  if (item.age >= 36 && item.age <= 55) {
+    return acc.numOldPeople++;
+  }
+  return { ...acc };
+});
+console.log(potential_voters);
