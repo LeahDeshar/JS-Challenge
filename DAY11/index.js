c0e583e6bd9b390e1a0fb1906cc0376ac9470061
Leah Deshar <leah.desar@gmail.com>
2022-08-31 18:29:45 +0545

    small changes

---
+++ DAY11/index.js
@@ -0,0 +1,26 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  //we are using data id in the html file
+  //to access this we can use dataset and
+  //more precise--> id
+  const id = e.target.dataset.id; //History,Vision,Goals
+  //buttons
+  //   console.log(id);
+  //only when we click on it
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    //then add the active class to thhe clicked btn only
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
